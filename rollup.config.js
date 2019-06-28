d28e9d69812563886927ac9be8a4bf27983a4ff5
Márton Visnovitz <vimtaai@inf.elte.hu>
2019-06-28 15:33:30 +0200

    Fix Rollup plugin order

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,15 @@
+const commonjs = require("rollup-plugin-commonjs");
 const resolve = require("rollup-plugin-node-resolve");
 const babel = require("rollup-plugin-babel");
 const minify = require("rollup-plugin-babel-minify");
-const commonjs = require("rollup-plugin-commonjs");
-const builtins = require("rollup-plugin-node-builtins");
 const json = require("rollup-plugin-json");
+const builtins = require("rollup-plugin-node-builtins");
 
 const { name: outputFileName, module: input } = require("./package.json");
 
 const isProductionBuild = process.env.BUILD === "production";
 const output = {};
-const plugins = [resolve({ preferBuiltins: false }), babel(), commonjs(), json(), builtins()];
+const plugins = [commonjs(), resolve({ preferBuiltins: false }), babel(), json(), builtins()];
 
 if (process.env.TARGET === "node") {
   output.format = "cjs";

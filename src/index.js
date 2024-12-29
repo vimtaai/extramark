b7cd68e24b4d65350d7b84d7efd7c0990a711ec7
Marton Visnovitz <visnovitz.marton@gmail.com>
2024-12-30 00:32:54 +0100

    fix: update prettier

--- src/index.js
+++ src/index.js
@@ -7,20 +7,13 @@ import sub from "markdown-it-sub";
 import abbr from "markdown-it-abbr";
 import footnote from "markdown-it-footnote";
 
-import critic from "./syntax/critic";
+import critic from "./syntax/critic.js";
 
 const parser = new MarkdownIt("commonmark");
 
 parser.set({ typographer: true });
 parser.enable("replacements").enable("table");
-parser
-  .use(anchor, [1, 2, 3])
-  .use(deflist)
-  .use(sup)
-  .use(sub)
-  .use(abbr)
-  .use(footnote)
-  .use(critic);
+parser.use(anchor, [1, 2, 3]).use(deflist).use(sup).use(sub).use(abbr).use(footnote).use(critic);
 
 export const parse = parser.parse.bind(parser);
 export const render = parser.render.bind(parser);

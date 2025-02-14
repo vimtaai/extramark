c80c7ea6616c40968072920e61f16b780dc07efa
Marton Visnovitz <visnovitz.marton@gmail.com>
2025-02-14 23:28:50 +0100

    ci: add release pipeline

---
+++ release.config.js
@@ -0,0 +1,12 @@
+/** @type {import('semantic-release').GlobalConfig} */
+export default {
+  branches: ["master"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    "@semantic-release/npm",
+    "@semantic-release/git",
+    "@semantic-release/github",
+  ],
+};

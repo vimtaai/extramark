a8b4548787062055dd797e9fba478e9a20f4a4a2
Marton Visnovitz <visnovitz.marton@gmail.com>
2025-02-14 22:32:46 +0100

    feat!: rewrite cli, remove input option

--- bin/extramark.js
+++ bin/extramark.js
@@ -1,74 +1,64 @@
-#!/usr/bin/env node
+import { error, log } from "node:console";
+import { mkdir, readFile, writeFile } from "node:fs/promises";
+import { dirname } from "node:path";
+import { argv, exit, stdin, stdout } from "node:process";
+import { styleText } from "node:util";
 
-import path from "node:path";
-import fs from "node:fs/promises";
-import process from "node:process";
-import program from "commander";
-
+import { program } from "commander";
 import { html } from "common-tags";
+
 import { render } from "../src/index.js";
-import { logger } from "./utils/logger.js";
 
 program
   .description("CLI for converting Markdown documents to HTML with ExtraMark")
   .usage("<input> [options]")
-  .option("-i --input [path]", "path of the input .md file")
   .option("-o --output [path]", "path of the output .html file")
   .option("-c --css [css]", "path to the CSS file to use for formatting")
   .option("-t --title [title]", "title of the output document")
   .option("-q --quiet", "omit console output")
-  .parse(process.argv);
+  .parse(argv);
 
-async function convert(program) {
-  const input = {
-    file: program.input || program.args[0] || process.stdin.fd,
-  };
+const options = program.opts();
+const inputFile = program.args[0];
+const outputFile = options.output;
+const data = {};
 
-  const output = {
-    dir: program.output ? path.dirname(program.output) : "",
-    file: program.output || process.stdout.fd,
-  };
-
-  if (program.quiet || output.file === process.stdout.fd) {
-    logger.disable();
+try {
+  if (inputFile) {
+    data.raw = await readFile(inputFile, "utf-8");
+  } else {
+    const buffer = await Array.fromAsync(stdin);
+    data.raw = buffer.toString();
   }
-
-  try {
-    logger.await("Reading input...");
-    input.data = await fs.readFile(input.file, "utf-8");
-  } catch (err) {
-    logger.error(`Could not read input file \`${input.file}\`.\n`, err);
-    return;
-  }
-
-  try {
-    logger.await("Generating output...");
-    output.data = html`
-      <!DOCTYPE html>
-      <meta charset="utf-8" />
-      <title>${program.title || `Markdown document`}</title>
-      ${program.css ? `<link rel="stylesheet" href="${program.css}">` : ``}
-      ${await render(input.data)}
-    `;
-  } catch (err) {
-    logger.error(`Could not parse input data.\n`, err);
-    return;
-  }
-
-  try {
-    await fs.mkdir(output.dir, { recursive: true });
-  } catch (err) {
-    logger.error(`Could not create output dir \`${output.dir}\`.\n`, err);
-    return;
-  }
-
-  try {
-    logger.await("Writing output...");
-    await fs.writeFile(output.file, output.data);
-    logger.success(`Created: ${output.file}`);
-  } catch (err) {
-    logger.error(`Could not write output file \`${output.file}\`.\n`, err);
-  }
+} catch {
+  error(`${styleText("red", "!")} Could not read input file '${inputFile}'.`);
+  exit(1);
 }
 
-convert(program);
+try {
+  data.parsed = html`
+    <!DOCTYPE html>
+    <meta charset="utf-8" />
+    <title>${program.title || `Markdown document`}</title>
+    ${program.css ? `<link rel="stylesheet" href="${program.css}">` : ``} ${await render(data.raw)}
+  `;
+} catch {
+  error(`${styleText("red", "!")} Could not parse input data.`);
+  exit(1);
+}
+
+try {
+  if (outputFile) {
+    await mkdir(dirname(outputFile), { recursive: true });
+    await writeFile(outputFile, data.parsed);
+  } else {
+    stdout.write(data.parsed + "\n");
+  }
+
+  if (outputFile && !options.quiet) {
+    log(`${styleText("green", "✔")} Created: ${outputFile}`);
+  }
+} catch (e) {
+  error(`${styleText("red", "!")} Could not write output file '${outputFile}'.`, e);
+  exit(1);
+}
--- bin/utils/logger.js
+++
@@ -1,3 +0,0 @@
-import Signale from "signale";
-
-export const logger = new Signale.Signale({ scope: "extramark", interactive: true });
